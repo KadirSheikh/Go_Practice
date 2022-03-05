d98ea6af9423e6dba6292878c41f2876323da82a
KadirSheikh <rs7246510@gmail.com>
2022-03-05 15:10:35 +0530

    MongoDB Fiber REST Api

--- RESTFul-API_MongoDB_Fiber/main.go
+++ RESTFul-API_MongoDB_Fiber/main.go
@@ -1,20 +1,54 @@
 package main
 
 import (
-	"restful_api_mongodb_fiber/configs"
-	"restful_api_mongodb_fiber/routes"
+	"log"
+	"os"
+
+	"mongo-go-api/config"
+	"mongo-go-api/routes"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/joho/godotenv"
 )
 
+func setupRoutes(app *fiber.App) {
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{
+			"success": true,
+			"message": "You are at the root endpoint 😉",
+		})
+	})
+
+	api := app.Group("/api")
+
+	routes.CatchphrasesRoute(api.Group("/catchphrases"))
+}
+
 func main() {
+
+	if os.Getenv("APP_ENV") != "production" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
+	}
+
 	app := fiber.New()
 
-	//run database
-	configs.ConnectDB()
+	app.Use(cors.New())
+	app.Use(logger.New())
 
-	//routes
-	routes.UserRoute(app)
+	config.ConnectDB()
 
-	app.Listen(":7000")
+	setupRoutes(app)
+
+	port := os.Getenv("PORT")
+	err := app.Listen(":" + port)
+
+	if err != nil {
+		log.Fatal("Error app failed to start")
+		panic(err)
+	}
 }
